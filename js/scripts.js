cf0f7520158bb0346ba4d6f75695dbeb1d4c42d1
jciarallij <jciarallij@gmail.com>
2016-03-14 12:01:25 -0400

    init commit
    function to add the cards together to find out the total for the player
    and dealer

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 // Setup a players starting hand
 // Place the card in the correct spot
 var theDeck = [];
-
+var placeInDeck = 0;
 $(document).ready(function(){
 	$('button').click(function(){
 		var clickedButton = ($(this).attr('id'));
@@ -19,18 +19,44 @@ $(document).ready(function(){
 });
 
 	function deal(){
-		theDeck = shuffleDeck();
+		shuffleDeck();
+		playerHand = [theDeck[0], theDeck[2] ];
+		dealerHand = [theDeck[1], theDeck[3] ];
+		placeInDeck = 4;
+		placeCard(playerHand[0], 'player', 'one');
+		placeCard(dealerHand[0], 'dealer', 'one');
+		placeCard(playerHand[1], 'player', 'two');
+		placeCard(dealerHand[1], 'dealer', 'two');
+		calculateTotal(playerHand, 'player');
+		calculateTotal(dealerHand, 'dealer');
+
 	}
 
-	function shuffle(){
+	function calculateTotal(hand, who){
+		var total = 0;
+		for(i = 0; i < hand.length; i++){
+			// purposely not fixing, 11, 12, or 13, or 1 = 11
+			var cardValue = hand[i].slice(0, -1);
+			console.log(cardValue);
+		}
+	}
+
+	function placeCard(card, who, slot){
+		var currId = '#' + who + '-card-' + slot;
+		$(currId).removeClass('empty');
+		$(currId).html(card);
+
+	}
+
+	function shuffleDeck(){
 		// fill the deck, in order, for now.
 		// Deck is made up of ....
 		// - 52 Cards + 5 random cards = 57 cards
 		// - 4 Suits
 		// --, h, s, d, c, r
 		// r = random Cards
-		s1 = hearts, s2 = spades, s3 = diamonds, s4 = clubs
-		for(s=1; s<=4; s++){
+		// s1 = hearts, s2 = spades, s3 = diamonds, s4 = clubs
+		for(var s = 1; s <= 4; s++){
 			var suit = "";
 			if(s === 1){
 				suit = 'h';
@@ -40,20 +66,31 @@ $(document).ready(function(){
 				suit = 'd';
 			}else if(s === 4){
 				suit = 'c';
+			// }else if (s === 5){
+			// 	suit = 'r';
 			}
 			for (i = 1; i<=13; i++){
 				theDeck.push(i+suit);
 			}
 		}
-		console.log(theDeck);
+		// console.log(theDeck);
+		// console.log(theDeck);
+		// Now, let's shuffle the deck.
+		var numberOfTimesToShuffle = 500;
+		for(i=1; i<numberOfTimesToShuffle; i++){
+			card1 = Math.floor(Math.random() * theDeck.length);
+			card2 = Math.floor(Math.random() * theDeck.length);
+			if(card1 != card2){
+				temp = theDeck[card1];
+				theDeck[card1] = theDeck[card2];
+				theDeck[card2] = temp;
+			}
+			
+		}
+
 }
 
-	// function hit(){
-
-	// }
 
 
 
-	// function stand(){
 
-	// }
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 #BlackJack game using jquery, HTML and CSS
 
 A BlackJack Game.
+
+
+###Setup the table using Bootstrap.
+
+Create a dealers hand and players hand to hold the totals
+Set each card to a custom width(adjust col-sm-2 to 11% to avoid having to write 3 extra divs)
+Add 3 buttons to the bottom of the table so the user can interact with the game.
+Bind each button to jQuery click function and call the correct function depending on the click.
